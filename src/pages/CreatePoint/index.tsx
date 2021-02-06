514ece848291b00e6561a605129597fd1182b184
Samanta Próspero <59126841+samantaProspero@users.noreply.github.com>
2021-02-06 10:30:50 -0300

    Update index.tsx

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -110,8 +110,8 @@ const CreatePoint = () => {
     event.preventDefault()
 
     const { name, email, whatsapp} = formData
-    const uf = selectedUf
-    const city = selectedCity
+    const uf = selectedUf || 'SP'
+    const city = selectedCity || 'São Paulo'
     const [latitude, longitude] = selectedPosition
     const items = selectedItems
 
